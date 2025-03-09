8357248566426882872ab5b2bd82b132822e2341
chenp <39854767+character76@users.noreply.github.com>
2025-03-09 19:21:44 +0100

    init_Dave_rig

---
+++ Assets/Kawaii Survivor/Scripts/Dace_move.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Dace_move : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rig;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
