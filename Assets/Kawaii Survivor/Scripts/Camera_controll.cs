8d70c4541e182f89ed0c33a0a832aae1c60b4441
chenp <39854767+character76@users.noreply.github.com>
2025-03-10 17:48:08 +0100

    Cam_controll_improve
    add frame for the cam movement

--- Assets/Kawaii Survivor/Scripts/Camera_controll.cs
+++ Assets/Kawaii Survivor/Scripts/Camera_controll.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Camera_controll : MonoBehaviour
 {
     [SerializeField] private Transform Target;
+    [SerializeField] private Vector2 minMaxXY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +21,9 @@ public class Camera_controll : MonoBehaviour
     {
         Vector3 tposition = Target.transform.position;
         tposition.z = -10;
+        tposition.x = Mathf.Clamp(tposition.x, -minMaxXY.x, minMaxXY.x);
+        tposition.y = Mathf.Clamp(tposition.y, -minMaxXY.y, minMaxXY.y);
+
         transform.position = tposition; 
     }
 }
