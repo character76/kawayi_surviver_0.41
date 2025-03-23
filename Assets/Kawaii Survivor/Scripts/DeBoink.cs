60623d2cc75a7bf3cf6817a2c6c9a0c3daa6f2cd
chenp <39854767+character76@users.noreply.github.com>
2025-03-23 20:49:48 +0100

    Weapon_aming

--- Assets/Kawaii Survivor/Scripts/DeBoink.cs
+++ Assets/Kawaii Survivor/Scripts/DeBoink.cs
@@ -16,20 +16,32 @@ public class DeBoink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AutoAim();
         
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position,range,enemyMask);
-        Vector2 targetUp = Vector3.up;
+        //Debug.Log("Closest Enemy" + cloestIndex + " dis " + minDis);
+       
+        
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position,range);
+    }
+    private Enemy_follow GetClosest()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyMask);
+        //Vector2 targetUp = Vector3.up;
 
-        if (enemies.Length<=0)
+        if (enemies.Length <= 0)
         {
-            transform.up = Vector3.up;
-            Debug.LogWarning("No cloest");
-            return;
+            
+            //Debug.LogWarning("No cloest");
+            return null;
         }
         int cloestIndex = -1;
         float minDis = range;
 
-        for(int i=0;i<enemies.Length;i++)
+        for (int i = 0; i < enemies.Length; i++)
         {
             Enemy_follow enemy_checking = enemies[i].GetComponent<Enemy_follow>();
             float disToenemy = Vector2.Distance(transform.position, enemy_checking.transform.position);
@@ -41,19 +53,26 @@ public class DeBoink : MonoBehaviour
             }
             else continue;
         }
-        if(cloestIndex==-1)
+        if (cloestIndex == -1)
         {
-            transform.up = Vector3.Lerp(transform.up, targetUp,Time.deltaTime*aimLerp);
-            return;
+            
+            return null;
         }
-        //Debug.Log("Closest Enemy" + cloestIndex + " dis " + minDis);
-       
-        targetUp= (enemies[cloestIndex].transform.position - transform.position).normalized;
-        transform.up = Vector3.Lerp(transform.up, targetUp, Time.deltaTime * aimLerp); 
+        return enemies[cloestIndex].GetComponent<Enemy_follow>();
     }
-    private void OnDrawGizmosSelected()
+    private void AutoAim()
     {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position,range);
+        Enemy_follow closest_Enemy = GetClosest();
+        Vector2 targetUp = Vector3.up;
+        if(closest_Enemy!= null)
+        {
+            targetUp = (closest_Enemy.transform.position - transform.position).normalized;
+        }
+        else
+        {
+
+        }
+        
+        transform.up = Vector3.Lerp(transform.up, targetUp, Time.deltaTime * aimLerp);
     }
 }
